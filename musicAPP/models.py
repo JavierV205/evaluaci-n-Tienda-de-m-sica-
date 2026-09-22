073b35e0aa62bc7969b4f14fddec4968d6610bdc
Javier <javiervalenzuelaco00@gmail.com>
2026-09-22 00:00:35 -0300

    Valida nombre, tipo, marca, cantidad y precio

---
+++ musicAPP/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+
+from django.core.validators import MaxValueValidator, MinValueValidator
+
+
+# Opciones para el campo 'tipo'
+TIPO_CHOICES = [
+        ('selecione', 'selecione'),
+        ('cuerda', 'Cuerda'),
+        ('viento', 'Viento'),
+        ('percusion', 'Percusion'),
+        ('teclado', 'Teclado'),
+    ]
+# Opciones para el campo 'marca'
+MARCA_CHOICES = [
+        ('selecione', 'selecione'),
+        ('yamaha', 'Yamaha'),
+        ('fender', 'Fender'),
+        ('gibson', 'Gibson'),
+        ('roland', 'Roland'),
+        ('otra', 'Otra'),
+    ]
+
+class Instrumentos(models.Model):
+    nombre=models.CharField(max_length=50)
+    tipo=models.CharField(max_length=50, 
+                          choices=TIPO_CHOICES, 
+                          default='selecione')
+    cantidad=models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(1000)])
+    precio=models.PositiveIntegerField(validators=[MaxValueValidator(1000000)])
+    marca=models.CharField( max_length=50, 
+                           choices=MARCA_CHOICES, 
+                           default='selecione')
+
